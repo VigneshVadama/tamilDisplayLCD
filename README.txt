a784b8eeadd43269af72f9b917ae7a9eda5861b4
Vignesh Parameswaran <vigneshmpc@gmail.com>
2012-10-21 20:20:48 +0530

    Latest Files
    tamilDisplay is the file which runs the Display

--- README.txt
+++ README.txt
@@ -7,6 +7,8 @@ The project is based on Arduino.
 
 The idea is based on "Hindi Font Set to be used on a 16x2 character LCD by-Rahul Kar"
 
+For easy visualisation, Arduino LCD Character Composer by Luis Aurelio is used.
+
 *********************************************************
 
 Will be updated..
--- tamilDisplay.ino
+++ tamilDisplay.ino
@@ -1,10 +1,14 @@
 #include <LiquidCrystal.h>
 #include <Tamil.h>
 LiquidCrystal lcd(8,9,10,11,12,13);
-void setup() {
+void setup()
+{
   lcd.begin(16,2);
   lcd.clear();
 }
-void loop() {
-  Tamil_Text(lcd,"asdfg");
+
+void loop()
+{
+   tamilDisplay(lcd,"fprPljegkauytHswd");
+//   tamilDisplay(lcd,"mmMnNzVbBicCcS");
 }
